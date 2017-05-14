c2152147400d6cb7c98e2f98c836b115a536203e
twinzpl <kacccer0@gmail.com>
2017-05-14 13:45:13 +0200

    Add files via upload

---
+++ js/fixed.js
@@ -0,0 +1,9 @@
+var num = 50; //number of pixels before modifying styles
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('#menu').addClass('fixed');
+    } else {
+        $('#menu').removeClass('fixed');
+    }
+});
